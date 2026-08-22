996014c4039ff5131554145defd3b9cbc16c01c3
Jeferson_san102 <sanchezjeferson477@gmail.com>
2026-08-22 11:23:43 -0600

    Clave usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,24 @@
 
 
 public class Usuario {
+    private String id;
+    private String clave;
+    private Rol rol;
+
+    public Usuario(){} //Constructor sin parametros
+    public Usuario(String id, String clave, Rol rol) {
+        this.id = id;
+        this.clave = clave;
+        this.rol = rol;
+    }
+
+    //Get's y set's
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getClave() {return clave;}
+    public void setClave(String clave) {this.clave = clave;}
+
+    public Rol getRol() {return rol;}
+    public void setRol(Rol rol) {this.rol = rol;}
 }
