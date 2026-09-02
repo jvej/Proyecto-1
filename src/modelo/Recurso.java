8ee0d99c8e54432d6778da319638afafdca34aa2
Axel Josue Montoya Ovares <233501606+axeljmoUNA@users.noreply.github.com>
2026-09-02 14:09:21 -0600

    Recurso

---
+++ src/modelo/Recurso.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public class Recurso {
+    private String id;
+    private String categoriaID;
+    private String descripcion;
+
+    public Recurso (){}
+    public Recurso (String id,String categoriaId, String descripcion){
+        this.id=id;
+        this.categoriaID=categoriaId;
+        this.descripcion = descripcion;
+    }
+
+    public String getId(){ return id;}
+    public String getCategoriaID(){ return categoriaID;}
+    public String getDescripcion(){ return descripcion;}
+
+    public void setId(String id){ this.id=id;}
+    public void setCategoriaIDId(String categoriaID){ this.id=categoriaID;}
+    public void setDescripcion(String descripcion){ this.descripcion=descripcion;}
+}
